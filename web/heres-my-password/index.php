3c68d2a867b531ea43d9735a247c866f66552697
Logan <65144990+Git-Logan@users.noreply.github.com>
2022-03-04 11:33:55 -0500

    Update index.php

--- web/heres-my-password/index.php
+++ web/heres-my-password/index.php
@@ -12,13 +12,19 @@ define("FLAG", "jctf{c0NGR@T2_y0U_p@22wORd_SPR@y3D!}");
 </head>
 
 <body>
+    <h1>Login</h1>
     <form action="" method="post">
-        Username: <input type="text" name="username">
+        <label>Username
+            <input type="text" name="username">
+        </label>
         <br>
-        Password: <input type="password" name="password">
+        <label>Password
+            <input type="password" name="password">
+        </label>
         <br>
         <input type="submit" value="Login" name="submit">
     </form>
+    <a href="forgot_password.php">Forgot Password?</a>
     <?php
     if (isset($_POST["submit"])) {
         if ($_POST["username"] === USERNAME && $_POST["password"] === PASSWORD) { ?>
